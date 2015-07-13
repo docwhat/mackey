4f82f852da6e64b53ad7132080f46f7abc4ac866
Christian Höltje <docwhat@gerf.org>
2015-07-13 14:26:11 -0400

    version the formula

--- mackey.rb
+++ mackey.rb
@@ -1,6 +1,9 @@
 class Mackey < Formula
+  desc "Shows info about your mac and a pretty logo"
   homepage "https://github.com/docwhat/mackey"
   head "https://github.com/docwhat/mackey.git"
+  url "https://github.com/docwhat/mackey/archive/v1.0.3.tar.gz"
+  sha256 "0b5725f1102292175e3c04d52d93bbe359f25109ff77f793b85f551a067d1bff"
 
   def install
     system "./configure"
