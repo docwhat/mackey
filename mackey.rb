5d5b06e2f42e0bbce851fd85f701f0f11c769127
Christian Höltje <choltje@us.ibm.com>
2016-04-08 10:13:54 -0400

    rubocop

--- mackey.rb
+++ mackey.rb
@@ -1,14 +1,17 @@
+# encoding: utf-8
+
+# Formula for Mackey
 class Mackey < Formula
-  desc "Shows info about your mac and a pretty logo"
-  homepage "https://github.com/docwhat/mackey"
-  head "https://github.com/docwhat/mackey.git"
-  url "https://github.com/docwhat/mackey/archive/v1.0.3.tar.gz"
-  sha256 "0b5725f1102292175e3c04d52d93bbe359f25109ff77f793b85f551a067d1bff"
+  desc 'Shows info about your mac and a pretty logo'
+  homepage 'https://github.com/docwhat/mackey'
+  head 'https://github.com/docwhat/mackey.git'
+  url 'https://github.com/docwhat/mackey/archive/v1.0.4.tar.gz'
+  sha256 'd3b6493cc7ec4184fd88f9fadf9ffa5e56e3f43cfc19814367759f18'
 
   def install
-    system "./configure"
-    bin.install "bin/mackey"
-    prefix.install "bundle"
+    system './configure'
+    bin.install 'bin/mackey'
+    prefix.install 'bundle'
   end
 
   test do
